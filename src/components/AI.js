4175ec248abab4c307e655bcf1c882a9127be7cd
Prince-Thind <princethind799@gmail.com>
2021-08-02 15:03:21 +0530

    add minimax tic tac toe package

--- src/components/AI.js
+++ src/components/AI.js
@@ -1,5 +1,31 @@
-function AIMove(cells){
-  return  Math.trunc(Math.random() * 9);
+import Minimax from 'tic-tac-toe-minimax';
+
+function AIMove(cells) {
+  const { ComputerMove } = Minimax;
+
+  const huPlayer = 'X';
+  const aiPlayer = 'O';
+  const symbols = {
+    huPlayer: huPlayer,
+    aiPlayer: aiPlayer,
+  };
+
+  const difficulty = 'Hard';
+
+  const board = [];
+
+  cells.forEach((cell, index) => {
+    if (cell === null) {
+      board[index] = index;
+    } else if (cell === 'X') {
+      board[index] = symbols.huPlayer;
+    } else {
+      board[index] = symbols.aiPlayer;
+    }
+  });
+
+  const nextMove = ComputerMove(board, symbols, difficulty);
+  return nextMove;
 }
 
 export default AIMove;
