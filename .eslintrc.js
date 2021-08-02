1f50ae2d5d500db43ec8cef363b08e8843f95f80
Prince-Thind <princethind799@gmail.com>
2021-08-02 14:17:55 +0530

    add eslint and webpack configs

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  //extends: ['airbnb-base'],
+  extends:['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-use-before-define': ['error', { functions: false }],
+  },
+};
